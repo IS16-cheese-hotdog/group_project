c4036478fa3dd1d80db2d1c4923b40f1c60c5337
tanaka-m <k022c0080@g.neec.ac.jp>
2025-01-16 16:01:37 +0900

    fix admin

---
+++ docker-test/src/inc/is_login.php
@@ -0,0 +1,9 @@
+<?php
+include_once __DIR__ . '/get_url.php';
+if (!isset($_SESSION['user_id'])) {
+    // 未ログインの場合
+    $url = get_url();
+    header('Location: ' . $url , '/login.php');
+    exit;
+}
+?>
