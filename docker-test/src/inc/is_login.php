137ef1c1d8fc855c4eba07588c44711de6565b3d
ok0693 <piyo2003918@gmail.com>
2025-01-19 19:43:53 +0900

    fix is_login.php

--- docker-test/src/inc/is_login.php
+++ docker-test/src/inc/is_login.php
@@ -1,6 +1,8 @@
 <?php
 include_once __DIR__ . '/get_url.php';
-if (!isset($_SESSION['user_id'])) {
+session_start();
+
+if (!isset($_SESSION['email'])) {
     // 未ログインの場合
     $url = get_url();
     header('Location: ' . $url . '/login.php');
